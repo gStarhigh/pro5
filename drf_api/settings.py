3741a796fdbfa439512c28ef725b134367ef6007
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-10 09:49:33 +0000

    created the post model

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'profiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
---
+++ posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    """
+    Post model.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=300)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_image', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
