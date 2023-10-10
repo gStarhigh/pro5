b81c6f093a69b8dd37b49f8fb1197d9a301fec9c
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-10 13:38:37 +0000

    made changes to allowed host and client origin dev due to heroku deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import re
 from pathlib import Path
 import os
 import dj_database_url
@@ -69,7 +69,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-gstarhigh-pro5-q3ji6zohki8.ws-eu105.gitpod.io',
-                 'https://pro5-aeecc7322fbc.herokuapp.com/']
+                 os.environ.get('ALLOWED_HOST')]
 
 
 # Application definition
@@ -117,9 +117,11 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''),
+                             re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
