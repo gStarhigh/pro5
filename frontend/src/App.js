141112e7ec8843e8b88173b05e3ec1ef8a964973
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-11 10:45:14 +0000

    set up the route for the navbar pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>}></Route>
+          <Route exact path="/signin" render={() => <h1>Sign in Page</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up page</h1>} />
+          <Route exact path="/contact" render={() => <h1>Contact Page</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
