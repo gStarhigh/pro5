a7b1c1a2d9427990e620e759282641b4c70f32b9
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-11 13:02:14 +0000

    added signupform and basic styles

---
+++ frontend/src/pages/auth/SignUpForm.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+
+const SignUpForm = () => {
+  return (
+    <Row className={styles.Row}>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
+      >
+        <Image
+          className={styles.SignInImage}
+          src={
+            "https://res.cloudinary.com/draygqe7t/image/upload/v1691501957/240594785_373990104267691_8073872753669794635_n_xammpg.jpg"
+          }
+        />
+      </Col>
+      <Col className="my-auto py-2 p-md-2" md={6}>
+        <Container className={`${styles.Content} p-4 `}>
+          <h1 className={styles.Header}>Sign up</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="Password1">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+            <Form.Group controlId="Password2">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+              />
+            </Form.Group>
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </Form>
+          <div className={`mt-3 ${styles.Content}`}>
+            <Link className={styles.Link} to="/signin">
+              Already have an account? <span>Sign in</span>
+            </Link>
+          </div>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
