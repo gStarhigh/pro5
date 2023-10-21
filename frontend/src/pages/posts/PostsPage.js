6364f9580a65e1a5da9250c09b349df90829c609
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-21 10:50:22 +0000

    500 error locally, when dev is on in env.py no 500 error. trying deployed version to see what happens there

--- frontend/src/pages/posts/PostsPage.js
+++ frontend/src/pages/posts/PostsPage.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Post from "./Post";
+import Loader from "../../components/Spinner";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function PostsPage() {
+import NoResults from "../../assets/no-results.png";
+
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2 text-center" lg={12}>
+      <Col
+        className="py-2 p-0 p-lg-2 text-center justify-content-center"
+        lg={12}
+      >
         <p>Information from users will go here in a list</p>
         <p>Navigation buttons here</p>
         <p>Popular profiles</p>
         <p>Search bar here</p>
         <p>List of posts here</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Loader src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Loader spinner />
+          </Container>
+        )}
       </Col>
     </Row>
   );
 }
-
 export default PostsPage;
