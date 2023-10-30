7a6513cec5d3daaba3729d1f25cb526ad395b324
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-30 10:40:31 +0000

    checks if the user is logged in in routes to prevent direct url access

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,10 @@ import { createContext } from "react";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
-import { useCurrentUser } from "./contexts/CurrentUserContext";
 import PostEditForm from "./pages/posts/PostEditForm";
+
+import { useCurrentUser } from "./contexts/CurrentUserContext";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -31,35 +33,49 @@ function App() {
               <PostsPage message="No results found. Adjust the search keyword." />
             )}
           />
-          <Route
-            exact
-            path="/feed"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or follow a user."
-                filter={`owner__followed__owner__profile=${profile_id}&`}
-              />
-            )}
-          />
-          <Route
-            exact
-            path="/liked"
-            render={() => (
-              <PostsPage
-                message="No results found. Adjust the search keyword or like a post."
-                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-              />
-            )}
-          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/contact" render={() => <h1>Contact Page</h1>} />
-          <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={() => <PostPage />} />
-          <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} />
+          {currentUser ? (
+            <>
+              <Route
+                exact
+                path="/posts/create"
+                render={() => <PostCreateForm />}
+              />
+              <Route exact path="/posts/:id" render={() => <PostPage />} />
+              <Route
+                exact
+                path="/posts/:id/edit"
+                render={() => <PostEditForm />}
+              />
+              <Route
+                exact
+                path="/feed"
+                render={() => (
+                  <PostsPage
+                    message="No results found. Adjust the search keyword or follow a user."
+                    filter={`owner__followed__owner__profile=${profile_id}&`}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/liked"
+                render={() => (
+                  <PostsPage
+                    message="No results found. Adjust the search keyword or like a post."
+                    filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                  />
+                )}
+              />
+            </>
+          ) : (
+            <Redirect to="/signin" />
+          )}
           <Route
             render={() => <h1 className="text-center">Page Not Found</h1>}
-          ></Route>
+          />
         </Switch>
       </Container>
     </div>
