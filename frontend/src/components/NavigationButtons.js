6f12856ef42eab8d58cfa80575a65989fd1c9158
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-30 10:15:39 +0000

    added currentuser to navigation buttons

--- frontend/src/components/NavigationButtons.js
+++ frontend/src/components/NavigationButtons.js
@@ -3,30 +3,36 @@ import { Link } from "react-router-dom";
 import btnStyles from "../styles/Button.module.css";
 import styles from "../styles/NavigationButtons.module.css";
 import { Container } from "react-bootstrap";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavigationButtons = () => {
+  const currentUser = useCurrentUser();
   return (
     <Container className={styles.Row}>
-      <Link to="/">
-        <button alt="To add usage" className={btnStyles.Round}>
-          Add Usage
-        </button>
-      </Link>
-      <Link to="/posts/create">
-        <button alt="To create post" className={btnStyles.Round}>
-          Create Post
-        </button>
-      </Link>
-      <Link to="/liked">
-        <button alt="To liked posts" className={btnStyles.Round}>
-          Liked Posts
-        </button>
-      </Link>
-      <Link to="/feed">
-        <button alt="To posts feed" className={btnStyles.Round}>
-          Posts Feed
-        </button>
-      </Link>
+      {currentUser ? (
+        <>
+          <Link to="/">
+            <button alt="To add usage" className={btnStyles.Round}>
+              Add Usage
+            </button>
+          </Link>
+          <Link to="/posts/create">
+            <button alt="To create post" className={btnStyles.Round}>
+              Create Post
+            </button>
+          </Link>
+          <Link to="/liked">
+            <button alt="To liked posts" className={btnStyles.Round}>
+              Liked Posts
+            </button>
+          </Link>
+          <Link to="/feed">
+            <button alt="To posts feed" className={btnStyles.Round}>
+              Posts Feed
+            </button>
+          </Link>
+        </>
+      ) : null}
     </Container>
   );
 };
