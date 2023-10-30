960e6c0ab742e535a93fef05d2ea66d7721ef46b
Gustaf Starhög <gustafstar@hotmail.com>
2023-10-30 11:08:34 +0000

    changed names and logo in manifest.json

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -9,7 +9,7 @@ const Avatar = ({ src, height = 45, text }) => {
         src={src}
         height={height}
         width={height}
-        alt="profile image"
+        alt="profile"
       ></img>
       {text}
     </span>
