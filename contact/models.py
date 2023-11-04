653db806e903ea3713f3d5650feb50982a398bef
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-04 13:18:22 +0000

    updated contact model

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.core.mail import send_mail
-import os
-from django.core.mail.message import BadHeaderError
+from django.contrib.auth.models import User
 
 
 class Contact(models.Model):
@@ -9,24 +7,13 @@ class Contact(models.Model):
     Contact model that lets the user contact the page owner on
     the frontend through their email.
     """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
     message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name + " - " + self.subject
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        self.send_email()
-
-    def send_email(self):
-        receiver_email = os.environ.get('EMAIL_HOST_USER')
-        send_mail(
-            self.subject,
-            self.message,
-            self.email,
-            [receiver_email],
-            fail_silently=False,
-        )
