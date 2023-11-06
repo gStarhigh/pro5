5915b2d52d4b3621b2c3a6a5efd5c8d00145a7ee
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-06 08:52:33 +0000

    added the is_owner boolean to information serializer

--- information/serializers.py
+++ information/serializers.py
@@ -4,8 +4,13 @@ from .models import Information
 
 class InformationSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.user
 
     class Meta:
         model = Information
         fields = ['user', 'start_date', 'end_date',
-                  'created_on', 'updated_on', 'text']
+                  'created_on', 'updated_on', 'text', 'is_owner',]
