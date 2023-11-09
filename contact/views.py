62359861b7f08597e8a0bd6a9f790b43e4a7a3ca
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-09 10:05:37 +0000

    created contact view and url

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets, generics, permissions
+from .models import Contact
+from .serializers import ContactSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class ContactViewSet(generics.ListCreateAPIView):
+    serializer_class = ContactSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        return Contact.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Edit or delete a contact ticket if you own it.
+    """
+    serializer_class = ContactSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return Contact.objects.all()
