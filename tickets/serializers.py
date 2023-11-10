16bcbafc1b9e8dd1728775089dc05ff88ba1a47f
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-10 08:36:57 +0000

    added ticketstatus to ticketserializer to display status not integer

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -5,10 +5,14 @@ from .models import Tickets
 class TicketsSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    ticket_status = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_ticket_status(self, obj):
+        return obj.get_ticket_status_display()
 
     class Meta:
         model = Tickets
