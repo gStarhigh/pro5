2fd926ba39c4b6cc92ee070a53951836f20e781b
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-13 10:02:38 +0000

    removed unused email settings and adjusted spinner.js style

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -181,16 +181,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Email
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -4,12 +4,9 @@ import React from "react";
 // React Bootstrap import
 import Spinner from "react-bootstrap/Spinner";
 
-// Styles
-import styles from "../styles/Asset.module.css";
-
 const Loader = ({ spinner, src, message }) => {
   return (
-    <div className={`${styles.Asset} p-4`}>
+    <div className={`p-4`}>
       {spinner && <Spinner animation="border" />}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
