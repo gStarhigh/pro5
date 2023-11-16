6d46fe493ed0affb50bb4c800a346faae8c76725
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-16 18:27:40 +0000

    added post to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'created_at', 'title',
+                    'description', 'content', 'image',)
+
+
+admin.site.register(Post, PostAdmin)
