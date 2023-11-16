b490f94db8ce807e3cb1de3ff318f2f6d027167d
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-16 18:24:29 +0000

    added tickets to admin panel

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Tickets
 
-# Register your models here.
+
+class TicketsAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'updated_on', 'created_on',
+                    'subject', 'message', 'ticket_status')
+
+
+admin.site.register(Tickets, TicketsAdmin)
