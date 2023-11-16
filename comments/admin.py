a48578127a0ee9d7277d2cde41d0f8cd92a5fa8d
Gustaf Starhög <gustafstar@hotmail.com>
2023-11-16 18:29:44 +0000

    added comment to admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'created_at', 'post', 'content',)
+
+
+admin.site.register(Comment, CommentAdmin)
